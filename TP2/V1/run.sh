1512b8e56100a7ad55537daf6f65e206db74d1f9
MarianaFilipa <marianarodrigues.mr116@gmail.com>
2023-04-15 21:51:00 +0100

    Alterações de versões intermédias dos trabalhos desenvolvidos

--- TP2/V1/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-module load gcc/7.2.0
-export CP_CLUSTERS=32
-
-perf stat -e instructions,cycles,L1-dcache-load-misses -M CPI make runpar
--- TP2/V2/README.md
+++
@@ -1,3 +0,0 @@
-# Descrição:
-
-Versão em que o número do cluster a que pertence cada ponto encontra-se num array separado
